3713783290562bed3bd23babdec515e3b9a67a7e
IIIRepublica <pepinoglande@gmail.com>
2013-09-02 10:46:48 +0200

    Better implementation

--- src/main/java/org/puzzles/rps/game/rules/RockPaperScissorsRules.java
+++ src/main/java/org/puzzles/rps/game/rules/RockPaperScissorsRules.java
@@ -1,21 +1,12 @@
 package org.puzzles.rps.game.rules;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class RockPaperScissorsRules implements Rules {
 
 	public List<Move> movesAllowed() {
-		RockPaperScissorsMoves[] moves = RockPaperScissorsMoves.values();
-		return createListOfMoves(moves);
-	}
-
-	private List<Move> createListOfMoves(RockPaperScissorsMoves[] moves) {
-		List<Move> movesAllowed = new ArrayList<Move>(moves.length);
-		for (int i = 0; i < moves.length; i++) {
-			movesAllowed.add(moves[i]);
-		}
-		return movesAllowed;
+		return Arrays.<Move>asList(RockPaperScissorsMoves.values());
 	}
 
 	public Move getByName(String name) {
